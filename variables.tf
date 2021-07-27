548b76d4535f678b5ada620ae2cb916ebd42f3e2
Sandeep-Bejjenki <bejjenki.sandeep78@gmail.com>
2021-07-27 17:34:37 +0530

    added variable file with main

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "azurerm_resource_group" {
+  description = "Name resource group"
+  default     = "sst02rg"
+}
+variable "azure_rg_location" {
+  description = "Resource group location"
+  default     = "East US"
+}
+variable "azurerm_virtual_network" {
+  description = "Name of Virtual Network"
+  default     = "sst01vnet"
+}
+variable "tags" {
+  description = "Tags for resources"
+  default     = {
+      CreatedBy = "Sandeep"
+      Env = "Dev"
+      Dept = "IT"
+  }
+}
+
+
+
