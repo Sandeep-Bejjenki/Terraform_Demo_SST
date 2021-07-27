219a2975edc0a02ba7d0d7ba7bccf39697cab2b4
Sandeep-Bejjenki <bejjenki.sandeep78@gmail.com>
2021-07-27 16:27:21 +0530

    Initial TF code

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg1" {
+  name     = "sst01rg"
+  location = "West Europe"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "sst01vnet"
+  resource_group_name = azurerm_resource_group.rg1.name
+  location            = azurerm_resource_group.rg1.location
+  address_space       = ["10.0.0.0/16"]
+}
