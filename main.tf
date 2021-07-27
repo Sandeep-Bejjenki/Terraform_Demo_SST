1f92eec5bba48b9e0f03dfe113e6cf3f51d7635d
Sandeep-Bejjenki <bejjenki.sandeep78@gmail.com>
2021-07-27 16:43:46 +0530

    Added subnet1

--- main.tf
+++ main.tf
@@ -14,4 +14,8 @@ resource "azurerm_virtual_network" "vnet1" {
   resource_group_name = azurerm_resource_group.rg1.name
   location            = azurerm_resource_group.rg1.location
   address_space       = ["10.0.0.0/16"]
+subnet {
+    name = "snet1"
+	address_prefix = "10.0.0.0/24"
+}
 }
